cd804e08504dff435be11aadc662a47cab26ea6e
TordTrøen <assessedgrub@gmail.com>
2015-02-24 13:00:51 +0100

    Added projectsturcture, gamemanager

---
+++ UnityProsjekt/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+	[HideInInspector]public Vector3 topRight; // Top right of screen in world coordinates
+	[HideInInspector]public Vector3 bottomLeft; // Bottom left of screen in world coordinates
+
+	void Awake()
+	{
+		UpdateScreenBounds ();
+	}
+
+	/// <summary>
+	/// Updates the screen bounds.
+	/// </summary>
+	public void UpdateScreenBounds()
+	{
+		Camera cam = Camera.main;
+		bottomLeft = cam.ScreenToWorldPoint (new Vector3 (0f, 0f));
+		topRight = cam.ScreenToWorldPoint (new Vector3 (cam.pixelWidth, cam.pixelHeight));
+	}
+}
