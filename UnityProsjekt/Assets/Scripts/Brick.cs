f6a6c3f587f0481b2d699e4a1e8509dba82e411c
TordTrøen <assessedgrub@gmail.com>
2015-02-27 15:34:56 +0100

    Added metadata

---
+++ UnityProsjekt/Assets/Scripts/Brick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour
+{
+	public int scoreReward = 1;
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		gameObject.SetActive (false);
+	}
+}
